f07e35aa2a845413dfb7349e0616dcd5d5acc7bd
User <user@iMac-CodeClan201.local>
2017-03-16 13:55:52 +0000

    Adds redirects and nested routes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { 
   BrowserRouter as Router, 
   Route,
-  NavLink 
+  NavLink,
+  Switch,
+  Redirect
 } from 'react-router-dom';
 
 import './App.css';
+
+const loggedIn = true;
 
 const Home = (props) => <h1>Home</h1>;
 
@@ -15,14 +19,27 @@ const isActive = (match, location) => {
   return match;
 }
 
+const Contact = ({match}) => (
+  <div>
+    <Route 
+      path="/contact" 
+      render={() => <h1>Contact</h1>} />
+  </div>
+)
+
 const Links = () => (
   <nav>
-    <NavLink exact activeClassName="active" to="/">Home</NavLink>
-    <NavLink activeStyle={{color: "green"}} to={{pathname: "/about"}}>About</NavLink>
+    <NavLink exact activeClassName="active" to="/?123">Home</NavLink>
+    <NavLink activeStyle={{color: "green"}} to={{pathname: "/about", search: "id=456"}}>About</NavLink>
     <NavLink 
       isActive={isActive} 
       activeClassName="active" 
-      replace to="/contact">Contact</NavLink>
+      replace to="/contact/?789">Contact
+    </NavLink>
+    <NavLink to="/old">
+      Old
+    </NavLink>
+    <NavLink to="/protected">Protected</NavLink>
   </nav>
 )
 
@@ -30,40 +47,54 @@ const App = (props) => (
   <Router>
   <div>
     <Links />
-    
-    <Route exact path="/" component={Home} />
-    {/*<Route strict path="/about/" render={() => <h1>About</h1>} />*/}
-    
-    <Route 
-      path="/about" 
-      children={({match}) => match && <h1>About</h1>} />
-    
-    <Route path="/contact" render={() => <h1>Contact</h1>} />
-    
-    <Route path="/:page/:subpage" render={({match}) => (
-      <h1>
-        PAGE: {match.params.page || 'Home'}<br />
-        SUBPAGE: {match.params.subpage}
-      </h1>
-    )} />
-    /* Use question mark to make param optional and hyphen to avoid making subdirectory. 
-    <Route path="/:page-:subpage?" render={({match}) => (
-      <h1>
-        PAGE: {match.params.page || 'Home'}<br />
-        SUBPAGE: {match.params.subpage}
-      </h1>
-    )} />
-    */
-    /* Use regular expressions to more precisely validate route parameters. e.g. \d+ for any number of digits, \d{2}-\d{2}-\d{4} for date format, \[a-z]+ for any number of alpha characters.
-    <Route path="/:a(\d+)/:b" render={({match}) => (
-      <h1>
-        PAGE: {match.params.a}<br />
-        SUBPAGE: {match.params.b}
-      </h1>
-    )} />
-    */
+    <Switch>
+        <Route exact path="/" component={Home} />
+        {/*<Route strict path="/about/" render={() => <h1>About</h1>} />*/}
+        <Route 
+          path="/about" 
+          children={({match}) => match && <div><h1>About</h1><br /><p>{new URLSearchParams(location.search).get("id")}</p></div>}
+           />
+
+        <Contact />
+
+        <Route path="/:page/:subpage" render={({match}) => (
+          <h1>
+            PAGE: {match.params.page || "Home"}<br />
+            SUBPAGE: {match.params.subpage}
+          </h1>
+        )} />
+      <Route render={() => <h1>Page not found</h1>} />
+      
+      <Route path="/old/:str" render={({match}) => (
+        <Redirect to={`/new/${match.params.str}`} />  
+      )} />
+
+      <Route path="/protected" render={() => (
+        loggedIn
+        ? <h1>Welcome to the protected page</h1>
+        : <Redirect to="/new/Login" />
+      )} />
+      
+    </Switch>
   </div>
   </Router>
 );
 
 export default App;
+
+/* Use question mark to make param optional and hyphen to avoid making subdirectory. 
+<Route path="/:page-:subpage?" render={({match}) => (
+  <h1>
+    PAGE: {match.params.page || 'Home'}<br />
+    SUBPAGE: {match.params.subpage}
+  </h1>
+)} />
+*/
+/* Use regular expressions to more precisely validate route parameters. e.g. \d+ for any number of digits, \d{2}-\d{2}-\d{4} for date format, \[a-z]+ for any number of alpha characters.
+<Route path="/:a(\d+)/:b" render={({match}) => (
+  <h1>
+    PAGE: {match.params.a}<br />
+    SUBPAGE: {match.params.b}
+  </h1>
+)} />
+*/
