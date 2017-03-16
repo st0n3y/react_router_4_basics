a78b9a414e13193d2ca00e9dc7546a8f77228c80
User <user@iMac-CodeClan201.local>
2017-03-16 11:19:30 +0000

    Adds links

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { 
+  BrowserRouter as Router, 
+  Route,
+  NavLink 
+} from 'react-router-dom';
 
-const Home = (props) => {
-  console.log(props);
-  return <h1>Home</h1>;
+import './App.css';
+
+const Home = (props) => <h1>Home</h1>;
+
+/* This function will be run every time links are rendered (i.e. route is changed) to determine if "contact" link qualifies for "active" class name. */
+const isActive = (match, location) => {
+  console.log(match, location);
+  return match;
 }
 
-const App = () => (
+const Links = () => (
+  <nav>
+    <NavLink exact activeClassName="active" to="/">Home</NavLink>
+    <NavLink activeStyle={{color: "green"}} to={{pathname: "/about"}}>About</NavLink>
+    <NavLink 
+      isActive={isActive} 
+      activeClassName="active" 
+      replace to="/contact">Contact</NavLink>
+  </nav>
+)
+
+const App = (props) => (
   <Router>
   <div>
+    <Links />
     <Route exact path="/" component={Home} />
     {/*<Route strict path="/about/" render={() => <h1>About</h1>} />*/}
     <Route 
       path="/about" 
       children={({match}) => match && <h1>About</h1>} />
+    <Route path="/contact" render={() => <h1>Contact</h1>} />
+    <Route path="/:page" render={({match}) => (
+      <h1>
+        PAGE: {match.params.page || 'Home'}
+      </h1>
+    )} />
   </div>
   </Router>
 );
